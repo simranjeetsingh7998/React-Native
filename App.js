59bf3632533ccdb9df89fdba198f963981d29be8
Simranjeet Singh <simran07kaler@gmail.com>
2020-05-22 12:33:36 +0530

    React Navigation Part-2

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Main from './components/MainComponent';
 
-export default class App extends Component {
+export default class App extends React.Component {
   render() {
     return (
       <Main />
